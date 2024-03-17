580267817dc76ed5040fc070a43d5d33e564b913
Pieter de Jong <pieter.a.dejong@gmail.com>
2024-03-17 19:10:58 -0400

    setup: update init script

--- init.sh
+++ init.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# create Python virtual environment
-python3 -m venv venv
+# USAGE: after running init.sh, don't forget to activate virtualenv: 'source venv/bin/activate'
+
+# create Python virtual environment if it doesn't exist
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
 
 # activate virtual environment
 source venv/bin/activate
@@ -9,6 +13,7 @@ source venv/bin/activate
 # install dependencies
 pip install -r requirements.txt
 
-# echo "Initialization complete!"
+# Additional setup steps can be added here
+
 echo
-echo -e "\033[0;32mInitialization complete!\033[0m"
+echo -e "\033[0;32mInitialization complete! Use 'source venv/bin/activate' to activate the virtual environment.\033[0m"
