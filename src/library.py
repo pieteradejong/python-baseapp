61f408b40b5f2aa6d841bb6cd80e325346bb3c48
Pieter de Jong <pieter.a.dejong@gmail.com>
2024-03-17 18:39:16 -0400

    ci, formatting: exclude library file from linting, and fix formatting

--- src/library.py
+++ src/library.py
@@ -10,15 +10,17 @@ Below: Usage for colors:
 
 from enum import Enum
 
+
 class TextColor(Enum):
-    RED = '\033[91m'
-    YELLOW = '\033[93m'
-    GREEN = '\033[92m'
-    PURPLE = '\033[95m'
-    MAGENTA = '\033[35m'
-    CYAN = '\033[96m'
-    BLUE = '\033[94m'
-    REGULAR = '\033[0m'
+    RED = "\033[91m"
+    YELLOW = "\033[93m"
+    GREEN = "\033[92m"
+    PURPLE = "\033[95m"
+    MAGENTA = "\033[35m"
+    CYAN = "\033[96m"
+    BLUE = "\033[94m"
+    REGULAR = "\033[0m"
+
 
 def print_colored(text: str, color: TextColor):
     print(f"{color.value}{text}{TextColor.END.value}")
@@ -36,6 +38,7 @@ print_colored("This is green text!", TextColor.GREEN)
 
 import csv
 
+
 def read_csv_file(file_path: str) -> list:
     """
     Reads a CSV file and returns a list of dictionaries, where each dictionary represents a row in the CSV,
@@ -45,12 +48,8 @@ def read_csv_file(file_path: str) -> list:
     :return: A list of dictionaries.
     """
     data = []
-    with open(file_path, mode='r', encoding='utf-8') as file:
+    with open(file_path, mode="r", encoding="utf-8") as file:
         reader = csv.DictReader(file)
         for row in reader:
             data.append(row)
     return data
-
-
-
-
